192bdfeced16ab0796456ea6ca12535f07eb7d55
dinethchatumina <dinethchatumina@gmail.com>
2025-08-23 19:50:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "aX4RSTpb#M3P1TBu3H4SdeT2o44CElpSVCmaIcVips5J2Re0ygUk",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
 BOT_OWNER: '947411336839',  // Replace with the owner's phone number
