3e828ade18df787de0fdd8ca71bbb24b771b3756
dinethchatumina <dinethchatumina@gmail.com>
2025-08-24 17:54:33 +0530

    Delete commands directory

--- commands/hello.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: "hello",
-  alias: ["hi", "hey"],
-  description: "Say hello",
-  category: "general",
-  async execute(m, sock, args) {
-    await sock.sendMessage(m.chat, { text: "Hello! කෙසේද?" }, { quoted: m });
-  },
-};
--- commands/hi.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: "hi",
-  alias: ["hello", "yo"],
-  description: "Send a friendly greeting",
-  category: "general",
-  async execute(m, sock, args, store) {
-    await sock.sendMessage(m.chat, { text: "Heyy! 😄 මොකද කෙරෙන්නේ?" }, { quoted: m });
-  },
-};
